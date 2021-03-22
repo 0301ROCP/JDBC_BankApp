1b931d0ca4758e1e4c105362253aab6e45b31cf1
Laurel German <laurel.german@gmail.com>
2021-03-22 18:54:46 -0400

    Add testing section to mainDriver

--- src/main/java/com/mybank/MainDriver.java
+++ src/main/java/com/mybank/MainDriver.java
@@ -1,17 +1,33 @@
 package com.mybank;
 
+import com.mybank.models.User;
 import com.mybank.presentation.controller.Controller;
-import com.mybank.util.ConnectionFactory;
+import com.mybank.repository.userdao.UserDaoImpl;
+import com.mybank.service.access_mgt.AccessMgrImpl;
 
 
 public class MainDriver {
 	
 	public static void main(String[] args) {
 				
-		Controller appController = new Controller();
 		
+		//--------TESTS-----------
+		
+//		UserDaoImpl uDao = new UserDaoImpl();
+//		AccessMgrImpl am = new AccessMgrImpl(uDao);
+		
+		
+		
+		
+		
+		//--------REAL CODE-----------
+		
+		Controller appController = new Controller();
+//		
 		appController.start();
 		
+		
+
 		
 		
 	}
